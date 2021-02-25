09d8114966e7c61b3283b8c39f11bb9bb1eeef3f
Steve <callasteven@gmail.com>
2021-02-24 20:49:49 -0700

    Minor formatting changes

--- src/main.js
+++ src/main.js
@@ -4,9 +4,8 @@ var inputText = document.querySelector(".text-box");
 var formInformation = document.querySelector("form");
 var renderIdeaBox = document.querySelector("#populatedIdea");
 var saveButton = document.querySelector('#saveButton');
-var starIcon = document.querySelector("#starIcon")
-var ideaBoxClass = document.querySelector(".idea-box-class")
-
+var starIcon = document.querySelector("#starIcon");
+var ideaBoxClass = document.querySelector(".idea-box-class");
 
 // Event Listeners
 formInformation.addEventListener("submit", submitNewIdea);
@@ -15,21 +14,8 @@ title.addEventListener("input", enableButton);
 inputText.addEventListener("input", enableButton);
 ideaBoxClass.addEventListener('click', favoritedStar);
 
-
-// go throught click one button, specific ID 
-// only button we want to hcange source on/ that specific ID
-// clicck another button have annother ID to target that button
-//to chang that source. in function need to assign queryslector
-//on the button i want. and run a functon when taht button is clicked
-//every ID should be unique. 
-//toggle
-
-
- 
-
-
 // global variables
-var newIdea = [];
+var newIdea;
 
 // functions below
 function submitNewIdea(event) {
@@ -39,24 +25,19 @@ function submitNewIdea(event) {
   renderIdea();
   clearTextBoxes();
   disableButton();
-
-  // formInputValidation();
-
-  //single responsibility function...
-};
+}
 
 function createIdeaList() {
   newIdea = new Idea(title.value, inputText.value);
-};
+}
 
 function updateIdeaList() {
   ideaList.unshift(newIdea);
-};
+}
 
 function renderIdea() {
     var createList = "";
     renderIdeaBox.innerHTML = "";
-
     for (var i = 0; i < ideaList.length; i++) {
     createList += `<div class="idea-boxes">
       <div class="idea-box-header">
@@ -79,54 +60,52 @@ function renderIdea() {
 function clearTextBoxes() {
   title.value = "";
   inputText.value = "";
-};
+}
 
 function loadWindow(event) {
   event.preventDefault();
   disableButton();
-  // disable button on load 
-};
+}
 
 function disableButton() {
   saveButton.disabled = true;
-};
+}
 
 function enableButton(event) {
   event.preventDefault();
-
   var monitorTitleInput = title.value;
   var monitorTextBoxInput = inputText.value;
-
   if (monitorTitleInput.length !== 0 && monitorTextBoxInput.length !== 0) {
     saveButton.disabled = false;
   }
   if (monitorTitleInput.length === 0 || monitorTextBoxInput.length === 0) {
     saveButton.disabled = true;
-  };
-};
+  }
+}
+
+function favoritedStar(event) {
+  var selectedStar = document.querySelector(`#${event.target.id}`);
+  newIdea.updateIsStar(`#${event.target.id}`);
+  console.log("1", `#${event.target.id}`);
+  if (ideaList[0].isStar === true) {
+    selectedStar.src = "./assets/star-active.svg";
+    console.log("2", `#${event.target.id}`);
+  } else {
+      selectedStar.src = "./assets/star.svg";
+  }
+}
 
 //udpate data model - when star is clicked, update data model
 // call the ideaClass.method determine if === false || true and toggle
 // off that. data model updates based off status, if statement to 
 // if star = true ... if star = false ... 
 
+// go throught click one button, specific ID 
+// only button we want to hcange source on/ that specific ID
+// clicck another button have annother ID to target that button
+//to chang that source. in function need to assign queryslector
+//on the button i want. and run a functon when taht button is clicked
+//every ID should be unique. 
+//toggle
 
-function favoritedStar(event) {
-  var selectedStar = document.querySelector(`#${event.target.id}`)
-
-  newIdea.updateIsStar(`#${event.target.id}`);
-    console.log("1", `#${event.target.id}`)
-  if (ideaList[0].isStar === true) {
-    selectedStar.src = "./assets/star-active.svg"
-    console.log("2", `#${event.target.id}`)
-  } else {
-    selectedStar.src = "./assets/star.svg"
-  }
-
-}
-
-// function switchStarIcon() {
-//   starIcon.src = "./assets/star-active.svg"
-//   console.log("star shold be here")
-// }
 
