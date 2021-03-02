2e1109e48ab2ea1548190604e31ae57f26f0c126
Steve <callasteven@gmail.com>
2021-03-01 20:20:18 -0700

    Various refactoring changes

--- src/Idea.js
+++ src/Idea.js
@@ -10,7 +10,64 @@ class Idea {
     this.errorIconURL = errorIconURL;
     this.altErrorIcon = "comment icon"
     this.commentText = commentText;
- }
+  }
+  
+  updateIdeaList(newIdea) {   
+    if (titleInput.value !== "" && textInput.value !== "") {                    //move to Idea class as updateIdeaList
+      ideaList.unshift(newIdea);
+    }
+  }
+
+  generateFavoriteIdeaList() {
+    var favoriteIdeaList = [];
+    for (var i = 0; i < ideaList.length; i++) {
+      if (ideaList[i].isStar === true) {
+        favoriteIdeaList.push(ideaList[i]);
+      }
+    }
+    renderFavoriteIdeaList(favoriteIdeaList)
+  }
+
+  updateIsStar(id) {
+    console.log(ideaList)
+    for (var i = 0; i < ideaList.length; i++) {
+      if (ideaList[i].id === parseInt(id) && ideaList[i].isStar === true) {
+       ideaList[i].isStar = false;
+       ideaList[i].urlStar = "./assets/star.svg";
+       ideaList[i].altStar = "small white star";
+      } else if (ideaList[i].id === parseInt(id) && ideaList[i].isStar === false){
+          ideaList[i].isStar = true;
+          ideaList[i].urlStar = "./assets/star-active.svg";
+          ideaList[i].altStar = "small red star";
+      }
+    }
+    this.saveToLocalStorage;
+  }
+
+  spliceIdeaBox(event) {
+      for (var i = 0; i < ideaList.length; i++) {
+        if (event.target.classList.contains("delete-icon") &&
+        (ideaList[i].id === parseInt(event.target.closest("article").id))) {
+          ideaList.splice(i, 1);
+      }
+    }
+  }
+
+  updateIsComment(id) {
+    for (var i = 0; i < ideaList.length; i++) {
+      if (ideaList[i].id === parseInt(id) && ideaList[i].isComment === true) {
+        ideaList[i].isComment = false;
+        ideaList[i].errorIconURL = "./assets/comment.svg";
+        ideaList[i].commentText = "Comment"
+        ideaList[i].altErrorIcon = "comment icon";
+      } else if (ideaList[i].id === parseInt(id) && ideaList[i].isComment === false){
+        ideaList[i].isComment = true;
+        ideaList[i].errorIconURL = "./assets/traffic sign-1.1s-200px.svg";
+        ideaList[i].commentText = "Under construction"
+        ideaList[i].altErrorIcon = "error icon functionality coming soon";
+      }
+    }
+  }
 
   saveToLocalStorage() {
     if(ideaList.length === 0) {
@@ -39,57 +96,6 @@ class Idea {
 
   deleteFromLocalStorage() {
     localStorage.clear()
-  }
-
-  updateIdea(id) {
-    console.log(ideaList)
-    for (var i = 0; i < ideaList.length; i++) {
-      if (ideaList[i].id === parseInt(id) && ideaList[i].isStar === true) {
-       ideaList[i].isStar = false;
-       ideaList[i].urlStar = "./assets/star.svg";
-       ideaList[i].altStar = "small white star";
-      } else if (ideaList[i].id === parseInt(id) && ideaList[i].isStar === false){
-          ideaList[i].isStar = true;
-          ideaList[i].urlStar = "./assets/star-active.svg";
-          ideaList[i].altStar = "small red star";
-      }
-    }
-    this.saveToLocalStorage;
-  }
-
-  spliceIdeaBox(event) {
-      for (var i = 0; i < ideaList.length; i++) {
-        if (event.target.classList.contains("delete-icon") &&
-        (ideaList[i].id === parseInt(event.target.closest("article").id))) {
-          ideaList.splice(i, 1);
-      }
-    }
-  }
-
-  updateComment(id) {
-    for (var i = 0; i < ideaList.length; i++) {
-      if (ideaList[i].id === parseInt(id) && ideaList[i].isComment === true) {
-        ideaList[i].isComment = false;
-        ideaList[i].errorIconURL = "./assets/comment.svg";
-        ideaList[i].commentText = "Comment"
-        ideaList[i].altErrorIcon = "comment icon";
-      } else if (ideaList[i].id === parseInt(id) && ideaList[i].isComment === false){
-        ideaList[i].isComment = true;
-        ideaList[i].errorIconURL = "./assets/traffic sign-1.1s-200px.svg";
-        ideaList[i].commentText = "Under construction"
-        ideaList[i].altErrorIcon = "error icon functionality coming soon";
-      }
-    }
-  }
-
-  generateFavoriteIdeaList() { //modified
-    var favoriteIdeaList = [];
-    for (var i = 0; i < ideaList.length; i++) {
-      if (ideaList[i].isStar === true) {
-        favoriteIdeaList.push(ideaList[i]);
-      }
-    }
-    renderFavoriteIdeaList(favoriteIdeaList)
   }
 
 }
